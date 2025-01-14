cf7a34be6b19eccfadf0b475133fa3aa766c42dc
AustinDecker <deckaj01@pfw.edu>
2025-01-14 13:07:36 -0500

    project1 code modifications

--- Project1/proj1.py
+++ Project1/proj1.py
@@ -2,6 +2,29 @@ def calc_compound_interest(initial_amount, annual_interest_rate, n_times_yearly_
     amount = float(initial_amount) * (1 + float(annual_interest_rate/n_times_yearly_compound)) ** (n_times_yearly_compound * years)
     return amount
 
+
+def get_user_input(prompt, answer_obj, valid_type="string"):
+    valid_input = False
+    match valid_type:
+        case "string":
+            response = str(input(prompt))
+            answer_obj["response"] = response
+            valid_input = True
+
+        case "number":
+
+            while not valid_input:
+                try:
+                    response = float(input(prompt))
+                    answer_obj["response"] = response
+                    valid_input = True
+                except ValueError:
+                    print("invalid input, please retype your response.")
+        case _:
+            print("feature not implemented")
+
+
+   
 p = float(input("initial balance: "))
 r = float(input("annual interest rate (ex 5% = 0.05): "))
 n = int(input("number of times compounded yearly: "))
